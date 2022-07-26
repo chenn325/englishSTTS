691aaaf658a6b638b84787d15f36fdf848d2b39d
chenn325 <hmz010325@gmail.com>
2022-07-26 14:11:56 +0800

    1

---
+++ english_project/app/src/main/java/com/example/english_project/net/IndexActivity.java
@@ -0,0 +1,148 @@
+package com.example.english_project.net;
+
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.english_project.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+public class IndexActivity extends AppCompatActivity {
+
+    EditText editTextUsername, editTextPassword;
+    TextView textViewHint;
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+        //if the user is already logged in we will directly start the profile activity
+        if(SharedPrefManager.getInstance(this).isLoggedIn()){
+            finish();
+            startActivity(new Intent(this, ProfileActivity.class));
+            return;
+        }
+
+        editTextUsername = (EditText) findViewById(R.id.editTextUsername);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        textViewHint = (TextView) findViewById(R.id.textViewHint);
+
+        findViewById(R.id.buttonRegister).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                registerUser();
+            }
+        });
+
+        findViewById(R.id.textViewLogin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //if user pressed on login
+                //we will open the login screen
+                finish();
+                startActivity(new Intent(IndexActivity.this, LoginActivity.class));
+            }
+        });
+    }
+
+    private void registerUser(){
+        final String username = editTextUsername.getText().toString().trim();
+        final String password = editTextPassword.getText().toString().trim();
+
+        //first we will do the validations
+        if(TextUtils.isEmpty(username)){
+            editTextUsername.setError("Please enter username");
+            editTextUsername.requestFocus();
+            return;
+        }
+        if(TextUtils.isEmpty(password)){
+            editTextPassword.setError("Enter a password");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        //if it passes all the validations
+
+        class RegisterUser extends AsyncTask<Void, Void, String> {
+            private ProgressBar progressBar;
+
+            @Override
+            protected String doInBackground(Void... voids){
+                //creating request handler object
+                RequestHandler requestHandler = new RequestHandler();
+
+                //creating request parameters
+                HashMap<String, String> params = new HashMap<>();
+                params.put("username", username);
+                params.put("password", password);
+
+                //returning the response
+                return requestHandler.sendPostRequest(URLs.URL_REGISTER, params);
+            }
+
+            @Override
+            protected void onPreExecute(){
+                super.onPreExecute();
+                //display the progress bar while user registers on the server
+                progressBar = (ProgressBar) findViewById(R.id.progressBar);
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            protected void onPostExecute(String s){
+                super.onPostExecute(s);
+                //hiding the progressbar after completion
+                progressBar.setVisibility(View.GONE);
+
+                try{
+
+                    //converting response to json object
+                    JSONObject obj = new JSONObject(s);
+
+                    //if no error in response
+                    if (!obj.getBoolean("error")) {
+                        Log.d("IndexActivity",obj.getString("message"));
+                        textViewHint.setText(obj.getString("message"));
+                        //getting the user from response
+                        JSONObject userJSON = obj.getJSONObject("user");
+
+                        //creating a new user object
+                        User user = new User(
+                                userJSON.getInt("id"),
+                                userJSON.getString("username")
+                        );
+
+                        //storing the user in shared preferences
+                        SharedPrefManager.getInstance(getApplicationContext()).userLogin(user);
+
+                        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                    } else {
+                        Log.d("IndexActivity",obj.getString("message"));
+                        textViewHint.setText(obj.getString("message"));
+                    }
+                }catch(JSONException e){
+                    e.printStackTrace();
+                    Log.d("IndexActivity","Index json error");
+                }
+            }
+        }
+
+        //executing the async task
+        RegisterUser ru = new RegisterUser();
+        ru.execute();
+
+    }
+
+}
---
+++ english_project/app/src/main/java/com/example/english_project/net/User.java
@@ -0,0 +1,21 @@
+package com.example.english_project.net;
+
+public class User {
+
+    private int id;
+    private String username;
+
+    public User(int id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+}
