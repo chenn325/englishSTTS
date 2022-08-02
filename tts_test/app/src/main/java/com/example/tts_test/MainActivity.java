3fb60f90190dd9e515f18d7a7ce663f14e6a2679
Octopus1031 <octopus891031@gmail.com>
2022-08-02 13:35:46 +0800

    tts_teach

--- tts_test/app/src/main/java/com/example/tts_test/MainActivity.java
+++ tts_test/app/src/main/java/com/example/tts_test/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.tts_test;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.speech.*;
 import android.speech.tts.TextToSpeech;
@@ -13,39 +16,55 @@ import android.view.View;
 import android.widget.*;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener{
 
-    ImageView mic;
-    EditText text;
-    Button sw;
+    ImageView mic, audio;
+    EditText inputAns;
+    TextView topicC, topicE, fb;
+    Button next;
+
+    TextToSpeech tts;
     private static final int RECOGNIZER_RESULT = 1;
+
+    //暫時題庫，未來用資料庫代替
+    String topicCDb[] = {"狗", "貓", "兔子"};
+    String topicEDb[] = {"dog", "cat", "rabbit"};
+    int QNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setTitle("語音轉文字");
-
         mic = findViewById(R.id.mic);
-        text = findViewById(R.id.text);
-        sw = findViewById(R.id.switchBut);
+        audio = findViewById(R.id.audio);
+        topicC = findViewById(R.id.topicC);
+        topicE = findViewById(R.id.topicE);
+        next = findViewById(R.id.next);
+        inputAns = findViewById(R.id.inputAns);
+        fb = findViewById(R.id.feedback);
 
-        //speech to text but
+        tts = new TextToSpeech(this, this);
+
         mic.setOnClickListener(micClick);
-        sw.setOnClickListener(swclick);
+        audio.setOnClickListener(audioClick);
+        next.setOnClickListener(nextclick);
 
-    }
+        next.setEnabled(false);
+        next.setVisibility(View.INVISIBLE);
 
-    public void onInit(int status){
-
+        QNum=0;
+        topicC.setText(topicCDb[QNum]);
+        topicE.setText(topicEDb[QNum]);
+        QNum++;
     }
 
     View.OnClickListener micClick = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            Log.d("MainActivity", "按");
+            audio.setEnabled(false);
             try {
                 Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
                 intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "en-US");
@@ -59,15 +78,30 @@ public class MainActivity extends AppCompatActivity {
                 ad.setPositiveButton("好", null);
                 ad.show();
             }
+            audio.setEnabled(true);
         }
     };
 
-    View.OnClickListener swclick = new View.OnClickListener(){
+    View.OnClickListener nextclick = new View.OnClickListener(){
         @Override
         public void onClick(View view) {
-            Intent i = new Intent();
-            i.setClass(MainActivity.this, MainActivity2.class);
-            startActivity(i);
+            topicC.setText(topicCDb[QNum]);
+            topicE.setText(topicEDb[QNum++]);
+            if(QNum>=topicCDb.length){
+                next.setText("題目已全作答完畢");
+                next.setEnabled(false);
+            }
+        }
+    };
+
+    View.OnClickListener audioClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            mic.setEnabled(false);
+            String s = topicE.getText().toString();
+//            tts.speak(s, TextToSpeech.QUEUE_FLUSH, null);
+            tts.speak(s, TextToSpeech.QUEUE_FLUSH, null, null);
+            mic.setEnabled(true);
         }
     };
 
@@ -76,9 +110,42 @@ public class MainActivity extends AppCompatActivity {
 
         if( requestCode==RECOGNIZER_RESULT && resultCode==RESULT_OK){
             ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-            text.setText(matches.get(0).toString());
+            String s = matches.get(0);
+            inputAns.setText(s);
+
+            if(s.equals(topicE.getText().toString())){
+                fb.setText("發音正確！ 可以練習下一題嘍～");
+                fb.setTextColor(this.getResources().getColor(R.color.blue));
+                next.setEnabled(true);
+            }
+            else{
+                fb.setText("發音可能不太對 再練習一下吧！");
+                fb.setTextColor(this.getResources().getColor(R.color.red));
+            }
         }
 
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onInit(int status) {
+        if(status == TextToSpeech.SUCCESS){
+            int result = tts.setLanguage(Locale.ENGLISH);
+            if(result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED){
+                Log.d("文字轉語音",  "不支援");
+            }
+        }
+        else{
+            Log.d("文字轉語音",  "初始化失敗");
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
     }
 }
